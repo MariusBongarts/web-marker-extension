6c57c5a46ae7fb5ecb40f3f413480a6c36c64327
Marius Bongarts <mb709027@fh-muenster.de>
2019-11-16 14:28:35 +0100

    CSS fixes; Autofocus of header search

--- components/web-marker/src/components/pop-up/add-directory-element/add-directory-element.component.ts
+++ components/web-marker/src/components/pop-up/add-directory-element/add-directory-element.component.ts
@@ -41,11 +41,12 @@ export class AddDirectoryComponent extends connect(store)(LitElement) {
     <div class="folder"
         @click=${() => {
             this.active = !this.active;
-            setTimeout(() => this.inputElement.focus(), 100)
+            setTimeout(() => this.inputElement.focus(), 50);
           }
           }
     >
-    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
+    <!-- <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg> -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
     </div>
     <div class="footer">
       <span>
--- components/web-marker/src/components/pop-up/header-toggle/header-toggle.component.ts
+++ components/web-marker/src/components/pop-up/header-toggle/header-toggle.component.ts
@@ -41,6 +41,11 @@ export class HeaderToggleComponent extends connect(store)(LitElement) {
     searchValueChanged(value);
   }
 
+  activateSearch() {
+    this.searchActive = !this.searchActive;
+    setTimeout(() => this.searchElement.focus(), 50);
+  }
+
   render() {
     return html`
     <div class="container">
@@ -80,7 +85,7 @@ export class HeaderToggleComponent extends connect(store)(LitElement) {
 
 </div>
 
-<div class="searchBtn" @click=${() => this.searchActive ? this.searchActive = false : this.searchActive = true}>
+<div class="searchBtn" @click=${() =>this.activateSearch()}>
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
   </div>
 </div>
