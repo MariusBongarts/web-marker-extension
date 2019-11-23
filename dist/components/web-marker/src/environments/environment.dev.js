5f3fad135165154864501f4ffa7b0db07b645a6a
Marius Bongarts <mb709027@fh-muenster.de>
2019-11-23 15:27:40 +0100

    Adjust manifest version of extension to 1.1.0

--- dist/components/web-marker/src/environments/environment.dev.js
+++ dist/components/web-marker/src/environments/environment.dev.js
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     BACKEND_URL: 'http://localhost:3000',
-    SOCKET_URL: 'http://localhost:3001'
+    SOCKET_URL: 'http://localhost:3001',
+    FRONTEND_URL: 'https://marius96.uber.space',
 };
 //# sourceMappingURL=environment.dev.js.map
